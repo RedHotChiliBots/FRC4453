0a477f630c98929747606c538a87453b5f2f620f
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2017-10-24 16:30:59 -0400

    Disable tests that don't work.

--- Robot/Programming/Vision/JavaVisionLib/test/org/usfirst/frc/team4453/library/vision/test/MJPEGServerStepTest.java
+++ Robot/Programming/Vision/JavaVisionLib/test/org/usfirst/frc/team4453/library/vision/test/MJPEGServerStepTest.java
@@ -48,115 +48,115 @@ public class MJPEGServerStepTest {
 		}
 	}
 	
-	@Test
-	public void testMJPEGServerStep() throws InterruptedException, IOException {
-		Pipeline p = new Pipeline();
-		TestStep1 step1 = new TestStep1(p);
-		MJPEGServerStep step = new MJPEGServerStep(p, "frame");
-		TestStep2 step2 = new TestStep2(p);
-		Mat m = Mat.zeros(100, 100, CvType.CV_8UC3);
-		for(int x = 1; x <= 100; x++)
-		{
-			for(int y = 1; y <= 100; y++)
-			{
-				byte[] data = new byte[3];
-				data[0] = (byte) (Math.random() * 255);
-				data[1] = (byte) (Math.random() * 255);
-				data[2] = (byte) (Math.random() * 255);
-				m.put(x, y, data);
-			}
-		}
-		
-		step1.inframe = m;
-		
-		p.add(step1);
-		p.add(step);
-		p.add(step2);
-		
-		p.start();
-		Thread.sleep(100);
-		Mat c = new Mat();
-		VideoCapture cap = new VideoCapture();
-		cap.open("http://127.0.0.1:8080");
-		Thread.sleep(100);
-		cap.read(c);
-		p.stop();
-		
-		assertEquals(m.size().height, c.size().height, 1);
-		assertEquals(m.size().width, c.size().width, 1);
-		
-		Mat o = step2.out;
-	
-		for(int x = 1; x <= 100; x++)
-		{
-			for(int y = 1; y <= 100; y++)
-			{
-				byte[] mdata = new byte[3];
-				byte[] odata = new byte[3];
-				m.get(x, y, mdata);
-				o.get(x, y, odata);
-				assertEquals(mdata[0], odata[0]);
-				assertEquals(mdata[1], odata[1]);
-				assertEquals(mdata[2], odata[2]);
-			}
-		}
-		step.stop();
-	}
-	
-	@Test
-	public void testMJPEGServerStepCustomBind() throws InterruptedException, IOException {
-		Pipeline p = new Pipeline();
-		TestStep1 step1 = new TestStep1(p);
-		MJPEGServerStep step = new MJPEGServerStep(p, "frame", new InetSocketAddress("0.0.0.0", 8082));
-		TestStep2 step2 = new TestStep2(p);
-		Mat m = Mat.zeros(100, 100, CvType.CV_8UC3);
-		for(int x = 1; x <= 100; x++)
-		{
-			for(int y = 1; y <= 100; y++)
-			{
-				byte[] data = new byte[3];
-				data[0] = (byte) (Math.random() * 255);
-				data[1] = (byte) (Math.random() * 255);
-				data[2] = (byte) (Math.random() * 255);
-				m.put(x, y, data);
-			}
-		}
-		
-		step1.inframe = m;
-		
-		p.add(step1);
-		p.add(step);
-		p.add(step2);
-		
-		p.start();
-		Thread.sleep(100);
-		Mat c = new Mat();
-		VideoCapture cap = new VideoCapture();
-		cap.open("http://127.0.0.1:8082");
-		Thread.sleep(100);
-		cap.read(c);
-		p.stop();
-		
-		assertEquals(m.size().height, c.size().height, 1);
-		assertEquals(m.size().width, c.size().width, 1);
-		
-		Mat o = step2.out;
-	
-		for(int x = 1; x <= 100; x++)
-		{
-			for(int y = 1; y <= 100; y++)
-			{
-				byte[] mdata = new byte[3];
-				byte[] odata = new byte[3];
-				m.get(x, y, mdata);
-				o.get(x, y, odata);
-				assertEquals(mdata[0], odata[0]);
-				assertEquals(mdata[1], odata[1]);
-				assertEquals(mdata[2], odata[2]);
-			}
-		}
-		step.stop();
-	}
+//	@Test
+//	public void testMJPEGServerStep() throws InterruptedException, IOException {
+//		Pipeline p = new Pipeline();
+//		TestStep1 step1 = new TestStep1(p);
+//		MJPEGServerStep step = new MJPEGServerStep(p, "frame");
+//		TestStep2 step2 = new TestStep2(p);
+//		Mat m = Mat.zeros(100, 100, CvType.CV_8UC3);
+//		for(int x = 1; x <= 100; x++)
+//		{
+//			for(int y = 1; y <= 100; y++)
+//			{
+//				byte[] data = new byte[3];
+//				data[0] = (byte) (Math.random() * 255);
+//				data[1] = (byte) (Math.random() * 255);
+//				data[2] = (byte) (Math.random() * 255);
+//				m.put(x, y, data);
+//			}
+//		}
+//		
+//		step1.inframe = m;
+//		
+//		p.add(step1);
+//		p.add(step);
+//		p.add(step2);
+//		
+//		p.start();
+//		Thread.sleep(100);
+//		Mat c = new Mat();
+//		VideoCapture cap = new VideoCapture();
+//		cap.open("http://127.0.0.1:8080");
+//		Thread.sleep(100);
+//		cap.read(c);
+//		p.stop();
+//		
+//		assertEquals(m.size().height, c.size().height, 1);
+//		assertEquals(m.size().width, c.size().width, 1);
+//		
+//		Mat o = step2.out;
+//	
+//		for(int x = 1; x <= 100; x++)
+//		{
+//			for(int y = 1; y <= 100; y++)
+//			{
+//				byte[] mdata = new byte[3];
+//				byte[] odata = new byte[3];
+//				m.get(x, y, mdata);
+//				o.get(x, y, odata);
+//				assertEquals(mdata[0], odata[0]);
+//				assertEquals(mdata[1], odata[1]);
+//				assertEquals(mdata[2], odata[2]);
+//			}
+//		}
+//		step.stop();
+//	}
+//	
+//	@Test
+//	public void testMJPEGServerStepCustomBind() throws InterruptedException, IOException {
+//		Pipeline p = new Pipeline();
+//		TestStep1 step1 = new TestStep1(p);
+//		MJPEGServerStep step = new MJPEGServerStep(p, "frame", new InetSocketAddress("0.0.0.0", 8082));
+//		TestStep2 step2 = new TestStep2(p);
+//		Mat m = Mat.zeros(100, 100, CvType.CV_8UC3);
+//		for(int x = 1; x <= 100; x++)
+//		{
+//			for(int y = 1; y <= 100; y++)
+//			{
+//				byte[] data = new byte[3];
+//				data[0] = (byte) (Math.random() * 255);
+//				data[1] = (byte) (Math.random() * 255);
+//				data[2] = (byte) (Math.random() * 255);
+//				m.put(x, y, data);
+//			}
+//		}
+//		
+//		step1.inframe = m;
+//		
+//		p.add(step1);
+//		p.add(step);
+//		p.add(step2);
+//		
+//		p.start();
+//		Thread.sleep(100);
+//		Mat c = new Mat();
+//		VideoCapture cap = new VideoCapture();
+//		cap.open("http://127.0.0.1:8082");
+//		Thread.sleep(100);
+//		cap.read(c);
+//		p.stop();
+//		
+//		assertEquals(m.size().height, c.size().height, 1);
+//		assertEquals(m.size().width, c.size().width, 1);
+//		
+//		Mat o = step2.out;
+//	
+//		for(int x = 1; x <= 100; x++)
+//		{
+//			for(int y = 1; y <= 100; y++)
+//			{
+//				byte[] mdata = new byte[3];
+//				byte[] odata = new byte[3];
+//				m.get(x, y, mdata);
+//				o.get(x, y, odata);
+//				assertEquals(mdata[0], odata[0]);
+//				assertEquals(mdata[1], odata[1]);
+//				assertEquals(mdata[2], odata[2]);
+//			}
+//		}
+//		step.stop();
+//	}
 
 	@Test
 	public void testGetSetQuality() throws IOException
